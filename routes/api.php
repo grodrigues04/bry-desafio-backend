ff488416786e13cbdc61fbd3e781401e85fc2576
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-05-24 17:54:06 -0300

    feat: Create route to employee and companies to edite

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ Route::get('/employees', [EmployeeController::class,'list_employees'])->name('')
 Route::post('/employees', [EmployeeController::class,'create_employee'])->name('');
 Route::get('/employees/{id}', [EmployeeController::class,'employee_id'])->name('');
 Route::patch('/employees/edit/{id}', [EmployeeController::class, 'employee_update'])->name('');
+Route::get('/employees/edit/{id}', [EmployeeController::class,'employee_id'])->name('employees.show');
 Route::delete('employees/delete/{id}',[EmployeeController::class,'employee_delete'])->name('');
 
 #Company
@@ -22,4 +23,5 @@ Route::get('/companies', [CompanyController::class,'list_companies'])->name('');
 Route::post('/companies', [CompanyController::class,'create_company'])->name('');
 Route::get('/companies/{id}', [CompanyController::class,'company_id'])->name('');
 Route::patch('/companies/edit/{id}', [CompanyController::class, 'company_update'])->name('');
+Route::get('/companies/edit/{id}', [CompanyController::class, 'company_id'])->name('');
 Route::delete('/companies/delete/{id}', [CompanyController::class,'company_delete'])->name('');
